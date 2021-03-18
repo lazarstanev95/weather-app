f6ee405cc019a4caab92bd005a1d95622bb74ba8
Lazar Stanev <ironman95@gmail.com>
2021-03-18 15:53:52 +0200

    Serve static files

--- server/app.js
+++ server/app.js
@@ -12,6 +12,14 @@ require(path.resolve('middlewares', 'index.middleware'))(app);
 //
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.get('/test', function(req, res) {
     res.json({
         message: 'success fetch from server'

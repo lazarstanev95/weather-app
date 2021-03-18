995a919384cbeef6bb4f061babaca79c4b867e68
Lazar Stanev <ironman95@gmail.com>
2021-03-18 18:13:16 +0200

    Fix deploy

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ require(path.resolve('server', 'middlewares', 'index.middleware'))(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/test', function(req, res) {
+    res.json({
+        message: 'success fetch from server'
+    })
+});
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + '/client/build/index.html'));
     })
 }
-
-app.get('/test', function(req, res) {
-    res.json({
-        message: 'success fetch from server'
-    })
-});
 
 module.exports = app;

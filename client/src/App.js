05e9758d8914df6879bafcb67932ad0677cc39a5
Lazar Stanev <ironman95@gmail.com>
2021-03-18 15:58:27 +0200

    test deploy

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={handleClick}>
-        test
+        test click button
       </button>
       <Weather />
     </div>
